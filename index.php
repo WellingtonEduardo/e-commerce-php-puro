a0e0249661e2c3c6dc8bdaab1d1d97febf018b29
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-07-10 12:10:21 -0300

    training xdebug

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 
-require_once "./src/Models/Teste.php";
-require_once "./src/Class/Teste.php";
-
-
-
-use treinamento\Class\Teste as ClassTeste;
-use treinamento\Models\Teste;
-
-$testePrint = new Teste();
-$testePrint->print();
-
-
-$testePrint = new ClassTeste();
-
-$testePrint->print();
+$stream = "./src/data/text.txt";
+$teste = file_get_contents($stream);
+var_dump($teste);
